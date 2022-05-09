66e7bb360e6b5befb024e4aad9748a1da933918b
Masih <haashemim@gmail.com>
2022-05-09 01:23:24 -0400

    "Change ratio alg to no reward alg"

--- lib/algorithms.py
+++ lib/algorithms.py
@@ -185,4 +185,49 @@ def freetime_ratio(env, Q, num_steps, epsilon, discount, alpha, alpha_f, toleran
             
     return Q, F, np.cumsum(rewards), np.cumsum(freetime_rewards)
         
+def freetime_no_reward(env, Q, num_steps, epsilon, discount, alpha, alpha_f, tolerance):
+    
+    F = np.ones_like(Q)
+
+    rewards = np.zeros(num_steps)
+    freetime_rewards = np.zeros(num_steps)
+    
+    state = env.reset()
+    
+    for step in tqdm(range(num_steps)):
         
+        action = select_epsilon_greedy_freetime(Q, F, state, epsilon)
+        
+        new_state, reward, terminal_state, info = env.step(action)
+        
+        if terminal_state:
+            q_target = reward
+        else: 
+            q_target = reward + discount * Q[new_state].max()
+        
+        if terminal_state:
+            if reward == 0:
+                freetime_reward = 0
+                freetime_target = 0
+            else:
+                freetime_reward = 1
+                freetime_target = 1
+        else:
+            new_Q = Q[new_state].max() * discount
+            old_Q = Q[state].max()
+            # Remove reward form freetime_reward equation
+            freetime_reward = 1 if  new_Q + tolerance >= old_Q else 0
+            freetime_target = freetime_reward + F[new_state].max() if freetime_reward == 1 else 0
+        
+        Q[state][action] = (1 - alpha) * Q[state][action] + alpha * q_target
+        F[state][action] = (1 - alpha_f) * F[state][action] + alpha_f * freetime_target
+        
+        rewards[step] = reward
+        freetime_rewards[step] = freetime_reward
+        
+        if terminal_state:
+            state = env.reset()
+        else: 
+            state = new_state
+            
+    return Q, F, np.cumsum(rewards), np.cumsum(freetime_rewards)
--- lib/run_comp.py
+++ lib/run_comp.py
@@ -159,14 +159,72 @@ def run(config):
             for title, F in F_tables.items():
                 plot_F(F, f'{title} init f-table freetime action {action}', config.f_plots.vmin, config.f_plots.vmax, 
                        action)
+        
+    print('Running no reward')
+    
+    results_no_reward = {
+        initialization: [] for initialization in INITIALIZATIONS
+    }
+    Q_tables = {}
+    F_tables = {}
+    
+    for initialization in INITIALIZATIONS:
+        
+        for run in range(config.num_runs):
+            Q = build_q_table(
+                (env.height, env.width),                
+                env.action_space.n, 
+                initialization = initialization, 
+                seed = config.random_initialization_seed, # type: ignore
+                max_reward = max_r
+            )
+            
+            Q, F, rewards, _ = freetime_no_reward(
+                env, 
+                Q, 
+                config.freetime.num_steps, 
+                config.freetime.epsilon, 
+                config.freetime.discount, 
+                config.freetime.alpha, 
+                config.freetime.alpha_f,
+                #config.offset
+                config.freetime.tolerance
+            )
+            
+            results_no_reward[initialization].append(rewards)
+            Q_tables[initialization] = Q
+            F_tables[initialization] = F
+        
+    if config.freetime.show_trajectory: 
+        for initialization, Q in Q_tables.items():
+            make_trajectory_map(Q, env, title=f'Init {initialization} freetime no_reward trajectory', 
+                                num_plots=config.trajectory_maps.num_plots)
+    
+    if config.freetime.show_rewards: 
+        plt.figure()
+        for title, rewards in results_no_reward.items():
+            plot_errorbars(rewards, label=title)
+            plt.legend()
+            plt.title('freetime no_reward')
+        
+    if config.freetime.show_q:
+        for title, Q in Q_tables.items():
+            plot_Q(Q, f'{title} init q-table freetime no_reward', config.q_plots.vmin, config.q_plots.vmax)
+        
+    if config.freetime.show_f:
+        for action in config.freetime.show_f_actions:
+            for title, F in F_tables.items():
+                plot_F(F, f'{title} init f-table freetime no_reward action {action}', config.f_plots.vmin, config.f_plots.vmax, 
+                       action)
+        
     if config.plot_freetime_vs_baseline_same_table:
         for initialization in INITIALIZATIONS:
             plt.figure()
             plot_errorbars(results_baseline[initialization], label='baseline')
             plot_errorbars(results_freetime[initialization], label='freetime')
+            plot_errorbars(results_no_reward[initialization], label='no_reward')
             plt.title(f'{initialization} initialization')
             plt.legend()
-        
-       
+            
     plt.show()
     
